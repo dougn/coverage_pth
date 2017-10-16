96499391091a81302ac3947536b573c21f571677
C. R. Oldham <cro@ncbt.org>
2017-10-16 10:53:37 -0600

    Expand regex for location of site-packages.  Provide more info to user about possible failure to find site-packages

--- setup.py
+++ setup.py
@@ -4,9 +4,18 @@ import re
 #from setuptools.dist import Distribution
 
 site_packages_path = sysconfig.get_python_lib()
-sprem = re.match(
-    r'.*(lib[\\/](python\d\.\d[\\/])?site-packages)', site_packages_path, re.I)
-rel_site_packages = sprem.group(1)
+try:
+    sprem = re.match(
+        r'.*(lib[\\/](python\d(\.\d)*[\\/])?site-packages)', site_packages_path, re.I)
+    if sprem is None:
+        sprem = re.match(
+            r'.*(lib[\\/](python\d(\.\d)*[\\/])?dist-packages)', site_packages_path, re.I)
+    rel_site_packages = sprem.group(1)
+except Exception as exc:
+    print("I'm having trouble finding your site-packages directory.  Is it where you expect?")
+    print("sysconfig.get_python_lib() returns '{}'".format(site_packages_path))
+    print("Exception was: {}".format(exc))
+    sys.exit(-1)
 
 #class PureDistribution(Distribution):
 #    def is_pure(self):
